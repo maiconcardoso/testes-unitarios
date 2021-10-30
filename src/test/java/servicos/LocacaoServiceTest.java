a36d56e67416010442865cc6c10b9f0b57ea51a4
Maicon Cardoso <maiconscardoso@hotmail.com>
2021-10-30 13:14:14 -0300

    Tratamento de Exceções

--- src/test/java/servicos/LocacaoServiceTest.java
+++ src/test/java/servicos/LocacaoServiceTest.java
@@ -1,8 +1,8 @@
 package servicos;
 
-
 import java.util.Date;
 
+import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,8 +15,8 @@ import br.ce.wcaquino.utils.DataUtils;
 public class LocacaoServiceTest {
 
     @Test
-	public void test() {
-		//cenario
+	public void test() throws Exception {
+		//cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
 		Filme filme = new Filme("Filme 1", 2, 5.0);
@@ -29,5 +29,31 @@ public class LocacaoServiceTest {
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
 		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
-    
+	@Test(expected = Exception.class)
+	public void testLocacao_filmeSemEstoque_Elegante() throws Exception {
+
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuário 2");
+		Filme filme = new Filme("Filme 2", 0, 5.0);
+
+		//ação
+		service.alugarFilme(usuario, filme);
+	}
+
+	@Test
+	public void testLocacao_filmeSemEstoque_Robusta(){
+
+		//cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuário 3");
+		Filme filme = new Filme("Filme 3", 2, 5.0);
+
+		//ação
+		try {
+			service.alugarFilme(usuario, filme);
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), Is.is("Filme sem estoque"));
+		}
+	}
 }
